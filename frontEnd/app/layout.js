1879f0af594fa9a2bbabbe229568544959a1a7da
Ariel M Diaz <arieldiaz.sistemas@gmail.com>
2025-02-12 11:26:49 -0300

    frontend: update the layout.js, create the nav, insert bootstrap

--- frontEnd/app/layout.js
+++ frontEnd/app/layout.js
@@ -1,7 +1,18 @@
+import Nav from './components/nav'; 
+import  "bootstrap/dist/css/bootstrap.min.css"
+
 export default function RootLayout({ children }) {
     return (
       <html lang="en">
-        <body>{children}</body>       
+        <head>
+          <title>Login and User Section</title>
+        </head>
+        <body className='bg-blue-100'>
+        <div className="bg-gray-100 p-4 rounded-lg shadow-md">
+          <Nav />
+          {children}
+        </div>
+        </body>       
       </html>
     );
   }
